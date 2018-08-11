f3771e44f8c86f9ac8492d2dff0849c357358328
sunjinshuai <s_mike@163.com>
2018-08-11 12:11:40 +0800

    add RandomWordsState 类

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-      ),
-       home: new Scaffold(
+      home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          //child: new Text('Hello World'),
-          child: new Text(wordPair.asPascalCase),
+          //child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
@@ -107,5 +104,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: new Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+  }
+  
+ 
+class RandomWordsState extends State<RandomWords> {
+    @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
